63cc1ace994ab9771a358acbfea14d91e3c9067f
Vu Huong <39198328+huongvu1397@users.noreply.github.com>
2018-10-03 07:50:02 +0700

    Thay đổi : - Thêm MapsActivity - Thêm Tạo Key - Tìm vị trí hiện tại - Đăng xuất - Chat - Profile User - upload image profile user

--- app/src/main/java/groups/kma/sharelocation/LoginAction/ActivityUser.java
+++ app/src/main/java/groups/kma/sharelocation/LoginAction/ActivityUser.java
@@ -1,9 +1,21 @@
 package groups.kma.sharelocation.LoginAction;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,30 +24,48 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import groups.kma.sharelocation.R;
 import groups.kma.sharelocation.model.Users;
 
 public class ActivityUser extends AppCompatActivity {
 
-    TextView tvUserName;
-    ImageView avatar;
+    TextView tvUsername1,tvUsername2,tvEmail;
+    CircleImageView imageUser;
 
-    private RecyclerView recyclerView;
     private DatabaseReference userDB;
     private FirebaseAuth mAuth;
+    private FirebaseStorage firebaseStorage;
+    private FirebaseDatabase firebaseDatabase;
+    private SharedPreferences preferences;
+
+    private static final int PICK_IMAGE_REQUEST = 1906;
+    private static final int STORAGE_PERMISSION_CODE = 1906;
+    private Uri imagePath;
+    private StorageReference storageReference;
+    private String temp = "";
+
     private Users myAccount;
     private Context context;
 
@@ -51,9 +81,40 @@ public class ActivityUser extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        mAuth = FirebaseAuth.getInstance();
-        userDB = FirebaseDatabase.getInstance().getReference(mAuth.getUid()).child("Users");
+        tvUsername1 = findViewById(R.id.username1);
+        tvUsername2 = findViewById(R.id.username2);
+        tvEmail = findViewById(R.id.emailuser);
+        imageUser = findViewById(R.id.imguser);
 
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        storageReference = firebaseStorage.getReference(mAuth.getUid());
+        storageReference.child("Profile").child("Avatar").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(imageUser);
+            }
+        });
+
+        userDB = firebaseDatabase.getReference(mAuth.getUid()).child("Users");
+
+        userDB.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Users users = dataSnapshot.getValue(Users.class);
+                tvUsername1.setText(users.getUserName());
+                tvUsername2.setText(users.getUserName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        preferences = getSharedPreferences("File_name", MODE_PRIVATE);
 
     }
 
@@ -69,14 +130,119 @@ public class ActivityUser extends AppCompatActivity {
 
 
     public void logOut(View view) {
+        Toast.makeText(ActivityUser.this, "Log Out", Toast.LENGTH_SHORT).show();
+        AlertDialog.Builder alert = new AlertDialog.Builder(ActivityUser.this);
+        alert.setTitle("Log out");
+        alert.setMessage("Do you want to log out?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //thoát khổi database và về màn hình chính
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(getApplicationContext(), ActivityDangNhap.class));
+                preferences.edit().putBoolean("saveLogin", false).apply();
+            }
+        });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                return;
+            }
+        });
+        alert.show();
     }
 
     public void ChangePass(View view) {
+        Toast.makeText(ActivityUser.this, "Change Password", Toast.LENGTH_SHORT).show();
     }
 
     public void ChangeEmail(View view) {
     }
 
     public void ChangeUsername(View view) {
+    }
+
+    public void chooseAva(View view) {
+        verifyPermission();
+    }
+    // lay anh trong file local
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imagePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imagePath);
+                imageUser.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver resolver = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(resolver.getType(uri));
+    }
+    //load ảnh lên firebase storage
+    private void uploadPro5Pic() {
+        if (imagePath != null) {
+            storageReference = FirebaseStorage.getInstance().getReference(mAuth.getUid());
+            StorageReference fileRef = storageReference.child("Profile").child("Avatar");
+            UploadTask uploadTask = fileRef.putFile(imagePath);
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(ActivityUser.this, "Upload thành công", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+    //
+    private void verifyPermission() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            openFileChooser();
+        } else {
+            requestStoragePermission();
+        }
+    }
+
+    private void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            permissionDialog();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
+    private void permissionDialog() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(ActivityUser.this);
+        alert.setTitle("Quyền truy cập");
+        alert.setMessage("Ứng dụng cần cấp quyền truy cập vào bộ nhớ của bạn");
+        alert.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ActivityCompat.requestPermissions(ActivityUser.this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+            }
+        });
+        alert.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alert.show();
+    }
+
+    public void btnSave(View view) {
+        uploadPro5Pic();
     }
 }
